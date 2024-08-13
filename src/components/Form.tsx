b26e027f145642a55bf5000463523835585814b8
PieMenard <pinedo.m.gustavo@gmail.com>
2024-08-13 01:16:36 -0400

    add input and form components

---
+++ src/components/Form.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRef } from 'react';
+
+type formProps = {
+  children: React.ReactNode;
+  action: (FormData: FormData) => void;
+  className?: string;
+  onSubmit?: () => void;
+};
+
+const Form = ({ children, action, className, onSubmit }: formProps) => {
+  const ref = useRef<HTMLFormElement>(null);
+  return (
+    <form
+      action={async (formData) => {
+        await action(formData);
+        ref.current?.reset();
+      }}
+      onSubmit={onSubmit}
+      ref={ref}
+    >
+      {children}
+    </form>
+  );
+};
+
+export default Form;
